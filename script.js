64eaf23f81cc40427447a8622db9e2be4b86c6c1
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2024-12-04 17:22:27 +0000

    feat(portfolio): begin pop up boxes

---
+++ script.js
@@ -0,0 +1,26 @@
+const buttons = document.querySelectorAll('.buttons-container > button');
+const infoBoxes = document.querySelectorAll('.info-box');
+
+infoBoxes.forEach((infoBox) => infoBox.classList.add('hide-info-box'))
+document.documentElement.style.overflowY = 'auto';
+document.documentElement.style.overflowX = 'hidden';
+
+function toggleInfoBox(itemClass) {
+  const infoBox = document.getElementById(itemClass);
+  infoBox.classList.toggle('hide-info-box')
+  infoBox.classList.toggle('show-info-box')
+  toggleScrollBar();
+}
+
+function toggleScrollBar() {
+  if (document.documentElement.style.overflowY == 'hidden') {
+    document.documentElement.style.overflowY = 'auto';
+  }
+  else {
+    document.documentElement.style.overflowY = 'hidden'
+  }
+}
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => toggleInfoBox(button.classList[0]))
+})

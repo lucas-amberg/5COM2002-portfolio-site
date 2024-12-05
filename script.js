00c7bd150d2f7248e4e07cabacaedd07b0073b4a
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2024-12-05 18:10:21 +0000

    feat(portfolio): add keybinds to close popups

--- script.js
+++ script.js
@@ -6,16 +6,35 @@ translucentBackground.classList.add('hide-translucent-background')
 document.documentElement.style.overflowY = 'auto';
 document.documentElement.style.overflowX = 'hidden';
 
+let prevItemClass = null;
+
 function toggleInfoBox(itemClass) {
   const infoBox = document.getElementById(itemClass);
-  infoBox.classList.toggle('hide-info-box')
-  infoBox.classList.toggle('show-info-box')
-  translucentBackground.classList.toggle('hide-translucent-background')
+  infoBox.classList.toggle('hide-info-box');
+  infoBox.classList.toggle('show-info-box');
+  translucentBackground.classList.toggle('hide-translucent-background');
   toggleScrollBar();
+  toggleEventListeners(itemClass);
 }
 
 function hideAllInfoBoxes() {
-  infoBoxes.forEach((infoBox) => infoBox.classList.add('hide-info-box'))
+  infoBoxes.forEach((infoBox) => infoBox.classList.add('hide-info-box'));
+}
+
+function handleKeyDown(event) {
+  if (event.ctrlKey && event.key === 'z') {
+    toggleInfoBox(prevItemClass);
+  } else if (event.key === 'Escape') {
+    toggleInfoBox(prevItemClass);
+  }
+}
+
+function toggleEventListeners(itemClass) {
+  if (prevItemClass) {
+    document.removeEventListener('keydown', handleKeyDown);
+  }
+  prevItemClass = itemClass;
+  document.addEventListener('keydown', handleKeyDown);
 }
 
 function toggleScrollBar() {
@@ -23,7 +42,6 @@ function toggleScrollBar() {
     document.documentElement.style.overflowY = 'auto';
   }
   else {
-    
     document.documentElement.style.overflowY = 'hidden'
   }
 }
